dd1582bb62297096a4a229e1379e07bdc9f77fc3
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2025-04-25 01:03:30 -0400

    Embed mini timer into tabs bar

--- src/components/event/EventDetails.tsx
+++ src/components/event/EventDetails.tsx
@@ -1,7 +1,11 @@
-import { Button, Container, Group, Loader, Tabs, Title } from '@mantine/core'
+import { Button, Container, Group, Loader, Tabs, TabsTabProps, Title } from '@mantine/core'
 import { useLiveQuery } from 'dexie-react-hooks'
 import { useNavigate, useParams } from 'react-router-dom'
-import { db } from '../../db'
+import { db, Timer } from '../../db'
+import eventCurrentRound from '../../db/event/event-current-round'
+import roundTimers from '../../db/round/round-timers'
+import useTimer from '../../hooks/use-timer'
+import { integerToColour } from '../../utils/colour'
 import MatchesTab from './matches/MatchesTab'
 import PlayersTab from './players/PlayersTab'
 import TimersTab from './timers/TimersTab'
@@ -10,7 +14,9 @@ export function EventDetails() {
   const navigate = useNavigate()
   const { id, tab } = useParams<{ id: string; tab: string | undefined }>()
 
-  const event = useLiveQuery(async () => id && db.events.get(id), [id])
+  const event = useLiveQuery(async () => (id ? db.events.get(id) : null), [id])
+  const currentRound = useLiveQuery(async () => event && eventCurrentRound(event), [event])
+  const timers = useLiveQuery(async () => currentRound && roundTimers(currentRound), [currentRound])
 
   if (!event) {
     return (
@@ -43,6 +49,11 @@ export function EventDetails() {
           <Tabs.Tab value="players">Players</Tabs.Tab>
           <Tabs.Tab value="matches">Matches</Tabs.Tab>
           <Tabs.Tab value="timers">Timers</Tabs.Tab>
+          {timers
+            ?.slice(0, 3)
+            ?.map((timer, index) => (
+              <TimerTab key={timer.id} timer={timer} ml={index === 0 ? 'auto' : undefined} inert />
+            ))}
         </Tabs.List>
 
         <Tabs.Panel value="players" pt="md">
@@ -58,5 +69,16 @@ export function EventDetails() {
         </Tabs.Panel>
       </Tabs>
     </Container>
+  )
+}
+
+function TimerTab({ timer, ...props }: { timer: Timer } & Omit<TabsTabProps, 'value'>) {
+  const { phase, hours, minutes, seconds } = useTimer(timer)
+
+  return (
+    <Tabs.Tab {...props} value={timer.id} c={integerToColour(phase?.colour ?? 0)}>
+      {hours > 0 ? `${hours.toString().padStart(2, '0')}:` : ''}
+      {minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}
+    </Tabs.Tab>
   )
 }
